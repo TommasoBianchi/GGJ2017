400583c57208888e3237c10c900b0b2ebad940c3
Unknown <jack-f@hotmail.it>
2017-01-21 04:46:51 +0100

    CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject Player;
+
+	void Update () {
+		gameObject.transform.position = Player.transform.position;
+		gameObject.transform.position  = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -50);
+	}
+}
